8ee56ee9ce92cd02cf25156bd3416e6f14ce8395
Alefer <alefernilson@gmail.com>
2025-10-25 13:03:14 -0300

    terceiro commit

---
+++ app_site/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.menu, name='menu'),  # página inicial do menu
+    path('cadastrar_colaborador/', views.cadastrar_colaborador, name='cadastrar_colaborador'),
+    path('cadastrar_equipamento/', views.cadastrar_equipamento, name='cadastrar_equipamento'),
+    path('visualizar_emprestimos/', views.visualizar_emprestimos, name='visualizar_emprestimos'),
+]
--- app_site/views.py
+++ app_site/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+def cadastrar_colaborador(request):
+    return render(request, 'app_site/pages/cadastrar_colaborador.html')
+
+def cadastrar_equipamento(request):
+    return render(request, 'app_site/pages/cadastrar_equipamento.html')
+
+def visualizar_emprestimos(request):
+    return render(request, 'app_site/pages/visualizar_emprestimos.html')
+
+def menu(request):
+    return render(request, 'app_site/pages/menu.html')  # 'menu.html' é o template da tela do menu

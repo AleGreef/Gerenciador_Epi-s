cef0c21c57f25d3cf097169a9a2452206ac41324
BRUNO SCHULLER <bruno_schuller@estudante.sesisenai.org.br>
2025-11-08 14:05:56 -0300

    Exibir mensagem de sucesso ou erro após confirmar no cadastro de colaboradores

--- app_site/urls.py
+++ app_site/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.menu, name='menu'),  # página inicial do menu
+    path('', views.menu, name='menu'), # página inicial do menu
+    path('gerenciar_col/', views.gerenciar_colaboradores, name='gerenciar_colaboradores'),
     path('cadastrar_colaborador/', views.cadastrar_colaborador, name='cadastrar_colaborador'),
+    path('verificar_cpf/', views.verificar_cpf, name='verificar_cpf'),
     path('cadastrar_equipamento/', views.cadastrar_equipamento, name='cadastrar_equipamento'),
     path('visualizar_emprestimos/', views.visualizar_emprestimos, name='visualizar_emprestimos'),
     path('lista_colaborador/', views.lista_colaborador, name='lista_colaborador'),
--- app_site/views.py
+++ app_site/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from app_site.models import Colaboradores, Setor
 from django.contrib import messages
-
-
+from django.http import JsonResponse
+from django.db import IntegrityError
+from app_site.models import Colaboradores, Setor
+from django.views.decorators.csrf import csrf_exempt
 
 def cadastrar_equipamento(request):
     return render(request, 'app_site/pages/cadastrar_equipamento.html')
@@ -14,31 +16,84 @@ def menu(request):
     return render(request, 'app_site/pages/menu.html')  # 'menu.html' é o template da tela do menu
 
 def cadastrar_colaborador(request):
-    setores = Setor.objects.filter(delete_flag='N')  # se depois quiser usar setor
+    setores = Setor.objects.filter(delete_flag='N')
+
+    # Valores padrão para repassar ao template
+    context = {
+        'setores': setores,
+        'cpf': '',
+        'nome': '',
+        'data_nascimento': '',
+        'telefone': '',
+        'email': '',
+        'senha': ''
+    }
 
     if request.method == 'POST':
         cpf = request.POST.get('cpf')
-        nome_colaborador = request.POST.get('nome')
-        data_nasc = request.POST.get('data_nascimento')
+        nome = request.POST.get('nome')
+        data_nascimento = request.POST.get('data_nascimento')
         telefone = request.POST.get('telefone')
         email = request.POST.get('email')
         senha = request.POST.get('senha')
 
-        Colaboradores.objects.create(
-            cpf=cpf,
-            nome_colaborador=nome_colaborador,
-            data_nasc=data_nasc,
-            telefone=telefone,
-            email=email,
-            senha=senha,
-            delete_flag='N'
-        )
+        # mantém os valores no contexto
+        context.update({
+            'cpf': cpf,
+            'nome': nome,
+            'data_nascimento': data_nascimento,
+            'telefone': telefone,
+            'email': email,
+            'senha': senha
+        })
 
-        messages.success(request, "Colaborador cadastrado com sucesso!")
-        return redirect('lista_colaborador')
+        # verifica duplicidade de CPF
+        if Colaboradores.objects.filter(cpf=cpf).exists():
+            messages.error(request, "Erro: Já existe um colaborador cadastrado com este CPF.")
+        else:
+            try:
+                Colaboradores.objects.create(
+                    cpf=cpf,
+                    nome_colaborador=nome,
+                    data_nasc=data_nascimento,
+                    telefone=telefone,
+                    email=email,
+                    senha=senha,
+                    delete_flag='N'
+                )
+                messages.success(request, "Colaborador cadastrado com sucesso!")
+            except IntegrityError:
+                messages.error(request, "Erro ao salvar o colaborador.")
 
-    return render(request, 'app_site/pages/cadastrar_colaborador.html', {'setores': setores})
+    return render(request, 'app_site/pages/cadastrar_colaborador.html', context)
+
 
 def lista_colaborador(request):
     colaboradores = Colaboradores.objects.all()
     return render(request, 'app_site/pages/menu.html', {'colaboradores': colaboradores})
+
+def verificar_cpf(request):
+    """Consulta AJAX: retorna dados do colaborador se o CPF já existir"""
+    cpf = request.GET.get('cpf')
+    colaborador = Colaboradores.objects.filter(cpf=cpf).first()
+
+    if colaborador:
+        data = {
+             'exists': True,
+              'colaborador': { 
+                'nome_colaborador': colaborador.nome_colaborador,
+                'data_nasc': colaborador.data_nasc.strftime("%Y-%m-%d") if colaborador.data_nasc else '',
+                'telefone': colaborador.telefone,
+                'email': colaborador.email,
+                'senha': colaborador.senha,
+        }
+    }
+    else:
+        data = {'exists': False}
+
+    return JsonResponse(data)
+
+
+def gerenciar_colaboradores(request):
+    colaboradores = Colaboradores.objects.all()  # busca todos do banco
+    return render(request, 'gerenciar_colaboradores.html', {'colaboradores': colaboradores})
